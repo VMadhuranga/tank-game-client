53c098d0bdd1e8f709d9af53c9fbb3679cb40449
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2025-06-24 08:45:29 +0530

    Update Boot scene

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -5,14 +5,41 @@ export class Boot extends Scene {
     super("Boot");
   }
 
-  create() {
+  async create() {
     if (!window["WebSocket"]) {
       this.add
         .text(10, 10, "Your browser does not support web socket")
         .setFontSize(50)
         .setPadding(10);
-    } else {
-      this.scene.start("MainMenu");
+
+      return;
     }
+
+    let isPlayable = false;
+    try {
+      const res = await fetch(document.location.origin + "/playable", {
+        method: "GET",
+      });
+
+      if (!res.ok) {
+        throw new Error();
+      }
+
+      const data = await res.json();
+      isPlayable = data.playable;
+    } catch (error: any) {
+      console.error(error.message);
+    }
+
+    if (!isPlayable) {
+      this.add
+        .text(10, 10, "Maximum player connection reached")
+        .setFontSize(50)
+        .setPadding(10);
+
+      return;
+    }
+
+    this.scene.start("MainMenu");
   }
 }
